76ca3bb47efaaca05d3fe6902b94c8ffb5ea6a2b
RGiebultowski <raf.giebultowski@gmail.com>
2021-09-02 15:16:40 +0200

    spa

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import {BrowserRouter, Link, Route} from 'react-router-dom';
 import './App.css';
 
-function App() {
+
+const Home = () =>{
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1>Home page</h1>
   );
+}
+
+const About = () =>{
+  return (
+    <h1>About page</h1>
+  );
+}
+
+const Contact = () =>{
+  return (
+    <h1>Contact page</h1>
+  );
+}
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div>
+          <header>
+            <nav>
+              <ul>
+                <li><Link to ="/">Home</Link></li>
+                <li><Link to ="/about">About</Link></li>
+                <li><Link to ="/contact">Contact</Link></li>
+              </ul>
+            </nav>
+          </header>
+          <section>
+            <Route path="/" exact={true} component={Home}/>
+            <Route path="/about" component={About}/>
+            <Route path="/contact" component={Contact}/>
+          </section>
+        </div>
+      </BrowserRouter>
+    );
+  }
+
 }
 
 export default App;
