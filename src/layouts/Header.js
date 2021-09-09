5fc71543cd31d8c1a453d6bcb4697f391e7f9242
RGiebultowski <raf.giebultowski@gmail.com>
2021-09-09 15:10:26 +0200

    nav changes

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import '../styles/Header.css';
+
+//images
+import img1 from '../img/3.jpg';
+import img2 from '../img/8_web.jpg';
+import img3 from '../img/9_web.jpg';
+import img4 from '../img/Dennis-4.jpg';
+import img5 from '../img/2.jpg';
+
 const Header = () => {
     return ( 
-        <div>Header</div>
+        <div>
+            <img src={img1} alt="m4" />
+        </div>
     );
 }
  
--- src/layouts/Navigation.js
+++ src/layouts/Navigation.js
@@ -1,7 +1,53 @@
 import React from 'react';
+import {NavLink} from 'react-router-dom';
+import '../styles/Nav.css';
+
+const list = [
+    {
+        name: "Strona główna",
+        path: "/",
+        exact: true
+    },
+    {
+        name: "Produkty",
+        path: "/products"
+    },
+    {
+        name: "Kontakt",
+        path: "/contact"
+    },
+    {
+        name: "Panel Admina",
+        path: "/admin"
+    },
+]
+
 const Navigation = () => {
+
+    const menu = list.map(item => (
+        <li key={item.name}>
+            <NavLink to={item.path} exact={item.exact ? item.exact : false}>{item.name}</NavLink>
+        </li>
+    ))
+
     return ( 
-        <div>Navigation</div>
+        <nav className="mainNav">
+            <ul>
+                {menu}
+                {/* <li>
+                    <NavLink to = "/"></NavLink>
+                </li>
+                <li>
+                    <NavLink to = "products"></NavLink>
+                </li>
+                <li>
+                    <NavLink to = "contact"></NavLink>
+                </li>
+                <li>
+                    <NavLink to = "admin"></NavLink>
+                </li> */}
+            </ul>
+        </nav>
     );
 }
  
